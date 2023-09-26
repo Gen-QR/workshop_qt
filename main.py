fabe1c87d4320ee3f1eae562714313bcf6ae23ae
Genevieve Anderson <60220079+Gen-QR@users.noreply.github.com>
2023-09-26 15:39:09 +0100

    feat: add some things to bare app

--- main.py
+++ main.py
@@ -1,16 +1,82 @@
 import sys
-from PySide2.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel
+from PySide2.QtWidgets import (
+    QApplication,
+    QCheckBox,
+    QComboBox,
+    QDateEdit,
+    QDateTimeEdit,
+    QDial,
+    QDoubleSpinBox,
+    QFontComboBox,
+    QLabel,
+    QLCDNumber,
+    QLineEdit,
+    QMainWindow,
+    QProgressBar,
+    QPushButton,
+    QRadioButton,
+    QSlider,
+    QSpinBox,
+    QTimeEdit,
+    QVBoxLayout,
+    QWidget,
+)
 
 
 class MyApp(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.initUI()
+        self.setWindowTitle("Widgets App")
 
-    def initUI(self):
-        self.setGeometry(100, 100, 400, 200)  # Set the window geometry (x, y, width, height)
-        self.setWindowTitle('PySide2 Workshop Template')
+        layout = QVBoxLayout()
+
+        my_label = QLabel("Look at all of these cool widgets!")
+        my_label.setStyleSheet("font-size: 16px; font-weight: bold;")
+        layout.addWidget(my_label)
+
+        widgets = [
+            QCheckBox,
+            QComboBox,
+            QDateEdit,
+            QDateTimeEdit,
+            QDial,
+            QDoubleSpinBox,
+            QFontComboBox,
+            QLCDNumber,
+            QLabel,
+            QLineEdit,
+            QProgressBar,
+            QPushButton,
+            QRadioButton,
+            QSlider,
+            QSpinBox,
+            QTimeEdit,
+        ]
+
+        for w in widgets:
+            layout.addWidget(w())
+
+        widget = QWidget()
+        widget.setLayout(layout)
+
+        # Set the central widget of the Window. Widget will expand
+        # to take up all the space in the window by default.
+        self.setCentralWidget(widget)
+
+        # Create a label
+        self.label = QLabel('Welcome to PySide2 Workshop!', self)
+        self.label.setGeometry(10, 10, 380, 30)  # Set the label's position and size
+
+        # Create a button
+        self.button = QPushButton('Click Me', self)
+        self.button.setGeometry(10, 50, 100, 30)  # Set the button's position and size
+
+        # Connect the button click event to a function
+        self.button.clicked.connect(self.show_message)
+
+    def show_message(self):
+        self.label.setText('Button Clicked!')
 
 
 def main():
